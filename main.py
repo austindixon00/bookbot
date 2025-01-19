3c0b61afa63a7cc9b61a2ad36a300091470cdd34
AustinDixon00 <austin.dixon2019@gmail.com>
2025-01-18 22:23:17 -0600

    totals characters and totals words

--- main.py
+++ main.py
@@ -1,28 +1,34 @@
 def main():
     
-#    print("Please Enter Book Title")
+#   print("Please Enter Book Title")
 #    x = input()
 #    input_file_path = (f"books/{x}.txt")
 #    file_path(input_file_path)
     
 
 #finds book to open
-#Path file can be changed to book_name
+#ath file can be changed to book_name
 #def file_path(book_name):
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     count_words_in_book(file_contents)
     
 def count_words_in_book(text):
-    word_count = 0
-    words = text.split()
-    for word in words:
-        word_count += 1
-    #print(word_count)
+    word_count = len(text.split())
+    print(word_count)
+    extracting_letters(text)
 
+def extracting_letters(text):
+    letter_count = {}
+    for char in text:
+        if char.isalpha() or char.isspace():
+            low_char = char.lower()
+            if low_char in letter_count:
+                letter_count[low_char] += 1
+            else:
+                letter_count[low_char] = 1
+    print(letter_count)
 
-    for letter in words:
-        print(words)
 
 
 #dict 
